858259144db9a7f63d26d8e4d4732ec61c779d49
easygoing0114 <easygoing0114@gmail.com>
2025-06-01 03:01:45 +0900

    modified:   js/main_test.js

--- js/main_test.js
+++ js/main_test.js
@@ -389,15 +389,6 @@
         document.head.appendChild(datalabelsScript);
       }
     }, 500); 
-
-    Defer(function () {
-      if (document.querySelector('canvas.chartjs')) {
-        const colorschemesScript = document.createElement('script');
-        colorschemesScript.src = 'https://cdn.jsdelivr.net/npm/chartjs-plugin-colorschemes@0.4.0/dist/chartjs-plugin-colorschemes.min.js';
-        colorschemesScript.async = true;
-        document.head.appendChild(colorschemesScript);
-      }
-    }, 500); 
   
   /* table の font-size と padding を画面の最大幅に合わせて変更 */
   Defer(function() {
