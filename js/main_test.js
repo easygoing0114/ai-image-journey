ba4849891610b4bca4821d68ca68a8b1a6a94080
easygoing0114 <easygoing0114@gmail.com>
2025-06-02 00:04:38 +0900

    modified:   js/main_test.js

--- js/main_test.js
+++ js/main_test.js
@@ -388,7 +388,9 @@
         datalabelsScript.async = true;
         document.head.appendChild(datalabelsScript);
       }
-    }, 500); 
+    }, 100); 
+
+
   
   /* table の font-size と padding を画面の最大幅に合わせて変更 */
   Defer(function() {
