a11039f21e32449ec366428627244a9c6d2aa735
easygoing0114 <easygoing0114@gmail.com>
2025-06-08 17:27:19 +0900

    modified:   js/main_test2.js

--- js/main_test2.js
+++ js/main_test2.js
@@ -494,7 +494,7 @@ if (document.querySelector('.chartjs') !== null) {
     getCurrentThemeColor();
     updateAllChartColors();
     Chart.register(ChartDataLabels);
-//    Chart.register(annotationPlugin);
+    Chart.register(annotationPlugin);
     
     // Get all canvas elements with class 'chartjs'
     const canvases = document.querySelectorAll('.chartjs');
