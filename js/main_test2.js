320199792f179b3e45f8dcdbc81d8faffade2683
easygoing0114 <easygoing0114@gmail.com>
2025-06-08 17:28:44 +0900

    modified:   js/main_test2.js

--- js/main_test2.js
+++ js/main_test2.js
@@ -17,7 +17,6 @@ if (document.querySelector('.mermaid') !== null) {
 if (document.querySelector('.chartjs') !== null) {
   Defer.js('https://cdn.jsdelivr.net/npm/chart.js/dist/chart.umd.min.js', 'chartjs', 100);
   Defer.js('https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.min.js', 'chartjsdatalabelsplugin', 1000);
-  Defer.js('https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-annotation/3.1.0/chartjs-plugin-annotation.min.js', 'chartjsannotationplugin', 1000);
   Defer.js('https://files.ai-image-journey.com/js/chartjs_arrow_plugin.js', 'chartjsarrowplugin', 1000);
 }
 if (document.querySelector('.markdown') !== null) {
@@ -494,7 +493,6 @@ if (document.querySelector('.chartjs') !== null) {
     getCurrentThemeColor();
     updateAllChartColors();
     Chart.register(ChartDataLabels);
-    Chart.register(annotationPlugin);
     
     // Get all canvas elements with class 'chartjs'
     const canvases = document.querySelectorAll('.chartjs');
