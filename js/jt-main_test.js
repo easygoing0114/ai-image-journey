61b7329b0f90d3bde08e6e51160866aa00a255d2
easygoing0114 <easygoing0114@gmail.com>
2025-06-25 06:14:45 +0900

    modified:   js/jt-main_test.js

--- js/jt-main_test.js
+++ js/jt-main_test.js
@@ -332,8 +332,7 @@ const jo = {};
     xhr[sendFn](null);
     xhr["add" + EventListenerFn](loadEvent, function() {
       var titleMatch = xhr[responseTextProp][matchFn](/<title>(.*?)<\/title>/);
-      var titleSeparator = " | ";
-      var blogTitle = "AI Image Journey";
+
       console.log("Original title:", titleMatch[1]);
       console.log("Separator + blogTitle:", titleSeparator + blogTitle);
       var cleanTitle = titleMatch[1][replaceFn](titleSeparator + blogTitle, "");
