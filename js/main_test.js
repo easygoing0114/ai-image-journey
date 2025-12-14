598c8cbd3de83c7a440e9c523b436195b10edf29
easygoing0114 <easygoing0114@gmail.com>
2025-12-15 01:39:28 +0900

    modified:   js/main_test.js

--- js/main_test.js
+++ js/main_test.js
@@ -203,7 +203,7 @@ if (document.querySelector('textarea') !== null) {
     const textareas = document.querySelectorAll('textarea');
 
     textareas.forEach(textarea => {
-      let isScheduled = false; // rAFが既にスケジュールされているかを管理
+      let isScheduled = false;
 
       function adjustHeight() {
         textarea.style.height = 'auto';
@@ -215,16 +215,12 @@ if (document.querySelector('textarea') !== null) {
       function scheduleAdjustHeight() {
         if (!isScheduled) {
           isScheduled = true;
-          // 次の描画フレーム直前で adjustHeight を実行
           requestAnimationFrame(adjustHeight);
         }
       }
 
-      // 初期ロード時に高さを設定
       scheduleAdjustHeight();
 
-      // イベント発生時に rAF に処理を委ねる
-      // rAF が既にスケジュールされていれば、不要な再スケジュールを防ぐ
       ['input', 'paste', 'cut', 'keydown', 'keyup'].forEach(event => {
         textarea.addEventListener(event, scheduleAdjustHeight);
       });
