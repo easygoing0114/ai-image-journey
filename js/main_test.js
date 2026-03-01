d75e7087e076346ad65b6650aca7c60f7c9f8747
easygoing0114 <easygoing0114@gmail.com>
2026-03-01 17:32:30 +0900

    modified:   js/main_test.js

--- js/main_test.js
+++ js/main_test.js
@@ -23,11 +23,11 @@ if (document.querySelector('#toc') !== null) {
 
 if (document.querySelector('.chartjs') !== null) {
   Defer.js('https://files.ai-image-journey.com/js/chart.umd.min.js', 'chartjs', 100);
-  Defer.js('https://files.ai-image-journey.com/js/chartjs-plugin-datalabels.min.js', 'chartjsdatalabelsplugin', 100);
-  Defer.js('https://files.ai-image-journey.com/js/chartjs-adapter-date-fns.bundle.min.js', 'chartjsadapterdatefns', 100);
-  Defer.js('https://files.ai-image-journey.com/js/chartjs_arrow_plugin.js', 'chartjsarrowplugin', 100);
-  Defer.js('https://files.ai-image-journey.com/js/papaprase.js', 'papapease', 100);
   Defer.js('https://files.ai-image-journey.com/js/chartjs-run.js', 'chartjs-run', 100);
+  Defer.js('https://files.ai-image-journey.com/js/chartjs-plugin-datalabels.min.js', 'chartjsdatalabelsplugin', 300);
+  Defer.js('https://files.ai-image-journey.com/js/chartjs-adapter-date-fns.bundle.min.js', 'chartjsadapterdatefns', 300);
+  Defer.js('https://files.ai-image-journey.com/js/chartjs_arrow_plugin.js', 'chartjsarrowplugin', 300);
+  Defer.js('https://files.ai-image-journey.com/js/papaprase.js', 'papapease', 300);
 }
 
 if (document.querySelector('.language-mermaid') !== null) {
