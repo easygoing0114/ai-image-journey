3075a960719e5f14c00bcfdc8e7d3ddc81330911
easygoing0114 <easygoing0114@gmail.com>
2026-03-02 18:21:16 +0900

    modified:   js/mermaid-run.js

--- js/mermaid-run.js
+++ js/mermaid-run.js
@@ -2,7 +2,6 @@ document.querySelectorAll('.mermaid-chart').forEach(figure => {
   figure.classList.add('box-img', 'box-img640');
 });
 
-const isDarkModeMermaid = document.documentElement.classList.contains('dark-mode');
 const today = new Date().toISOString().split('T')[0];
 
 // ユーティリティ関数（変更なし）
@@ -156,7 +155,7 @@ Defer(function () {
   // 初回描画時の設定
   mermaid.initialize({
     startOnLoad: false,
-    theme: isDarkModeMermaid ? 'dark' : 'default',
+    theme: isDarkMode ? 'dark' : 'default',
   });
 
   // 日付更新済みのDOMコードを使って描画を実行
